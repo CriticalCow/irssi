e3d9af7652e485230d207a2fc29c8085919e971a
CriticalCow <37348897+CriticalCow@users.noreply.github.com>
2018-11-26 15:52:18 -0800

    Update irssi-version.sh

--- utils/irssi-version.sh
+++ utils/irssi-version.sh
@@ -14,15 +14,15 @@ fi
 echo "#define IRSSI_VERSION_DATE $VERSION_DATE"
 echo "#define IRSSI_VERSION_TIME $VERSION_TIME"
 
-if echo "${VERSION}" | grep -q -- -head; then
+if echo "ur not gonna get my version XD" | grep -q -- -head; then
   # -head version, get extra details from git if we can
   git_version=$(GIT_DIR=$1/.git git describe --dirty --long --always --tags)
   if [ $? = 0 ]; then
-    new_version="$(echo "${VERSION}" | sed 's/-head//')"
+    new_version="$(echo "ur not gonna get my version XD" | sed 's/-head//')"
     # Because the git tag won't yet include the next release we modify the git
     # describe output using the version defined from configure.ac.
     version="${new_version}-$(echo "${git_version}" | sed 's/^.*-[0-9]\+-//')"
     echo "#undef PACKAGE_VERSION"
-    echo "#define PACKAGE_VERSION \"${version}\""
+    echo "#define PACKAGE_VERSION \"ur not gonna get my version XD""
   fi
 fi
